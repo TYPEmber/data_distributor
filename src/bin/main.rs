f0d7ac8baaaac6faaafb29747f377ef475655b8e
Amadeus <nono02200059@gmail.com>
2021-03-07 11:32:09 +0800

    initial

---
+++ src/bin/main.rs
@@ -0,0 +1,140 @@
+use data_distributor::*;
+#[tokio::main]
+async fn main() {
+    let stop_sender = crate::initial().await;
+    recv_pkg("127.0.0.1:19208".parse().unwrap(), 100_000_0).await;
+    send_pkg("127.0.0.1:5503".parse().unwrap(), 100_000_0, 5e8).await;
+
+    stop_sender.send(());
+
+    loop {
+        tokio::time::sleep(tokio::time::Duration::from_millis(1_000)).await;
+    }
+}
+
+async fn test_socket_send_limited() {
+    let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(5_000));
+    let loop_times = 200_000_0;
+
+    let one_socket_cost = tokio::spawn(async move {
+        let socket = generate_socket("0.0.0.0:0".parse().unwrap(), 1024, 1024 * 1024 * 10);
+        let addr: std::net::SocketAddr = "127.0.0.1:12345".parse().unwrap();
+        let data = [0u8; 1350];
+
+        let mut watch = std::time::SystemTime::now();
+        let mut i = 0usize;
+        while i < loop_times {
+            socket.send_to(&data, addr).await;
+            i += 1;
+        }
+        watch.elapsed().unwrap().as_secs_f64()
+    })
+    .await
+    .unwrap();
+
+    let multi_count = 5usize;
+
+    let mut tasks = vec![];
+    for k in 0..multi_count {
+        let socket = generate_socket("0.0.0.0:0".parse().unwrap(), 1024, 1024 * 1024 * 10);
+        let addr: std::net::SocketAddr = "127.0.0.1:12345".parse().unwrap();
+        let data = [0u8; 1350];
+
+        tasks.push(tokio::spawn(async move {
+            let mut watch = std::time::SystemTime::now();
+            let mut i = 0usize;
+            while i < loop_times / multi_count {
+                socket.send_to(&data, addr).await;
+                i += 1;
+            }
+            watch.elapsed().unwrap().as_secs_f64()
+        }));
+    }
+    let mut multi_socket_cost = 0f64;
+    for t in tasks {
+        multi_socket_cost += tokio::join!(t).0.unwrap();
+    }
+
+    println!(
+        "{:?} {} bps",
+        one_socket_cost,
+        (loop_times * 1350 * 8) as f64 / one_socket_cost
+    );
+    println!(
+        "{:?} {} bps",
+        multi_socket_cost,
+        (loop_times * 1350 * 8) as f64 / multi_socket_cost
+    );
+    panic!();
+}
+
+use socket2::{Domain, SockAddr, Socket, Type};
+
+async fn send_pkg(addr: std::net::SocketAddr, pkg_count: usize, speed_rate: f64) {
+    let any_addr: std::net::SocketAddr = "0.0.0.0:0".parse().unwrap();
+    let mut socket = crate::generate_socket(any_addr, 4096, 4096 * 1024);
+
+    let mut time = std::time::SystemTime::now();
+    let mut last_print_time = 0usize;
+    let mut send_bits_count_last = 0usize;
+    let mut send_bits_count = 0usize;
+    let mut send_pkg_count = 0usize;
+    let mut speed_now = 0.0;
+
+    let data = [0u8; 1350];
+
+    let mut watch = std::time::SystemTime::now();
+
+    while send_pkg_count < pkg_count {
+        let mut time_f64 = time.elapsed().unwrap().as_secs_f64();
+        if send_pkg_count % 1 == 0 {
+            speed_now = send_bits_count as f64 / time_f64;
+            //println!("{}", time.elapsed().unwrap().as_secs_f64());
+            while (speed_now > speed_rate) {
+                tokio::time::sleep(tokio::time::Duration::from_millis(1)).await;
+                speed_now = send_bits_count as f64 / time.elapsed().unwrap().as_secs_f64()
+            }
+        }
+
+        let time_usize = time_f64 as usize;
+
+        if time_usize - last_print_time >= 1 {
+            println!("{:?}", speed_now);
+            last_print_time = time_usize;
+        }
+
+        if let Ok(len) = socket.send_to(&data, addr).await {
+            if len != 1350 {
+                panic!();
+            }
+            send_bits_count += len * 8;
+        }
+        send_pkg_count += 1;
+    }
+
+    let du = watch.elapsed().unwrap().as_secs_f64();
+
+    println!(
+        "send_pkg_count: {} speed: {} duration: {}",
+        send_pkg_count, speed_now, du
+    );
+}
+
+async fn recv_pkg(addr: std::net::SocketAddr, pkg_count: usize) {
+    let mut socket = crate::generate_socket(addr, 4096 * 4096 * 10, 4096);
+
+    let mut recv_pkg_count = 0usize;
+
+    tokio::spawn(async move {
+        let mut buffer = [0u8; 1500];
+        while recv_pkg_count < pkg_count {
+            socket.recv(&mut buffer).await;
+            recv_pkg_count += 1;
+
+            if recv_pkg_count % 10000 == 0 {
+                println!("has recv: {}", recv_pkg_count);
+            }
+        }
+        println!("mission complete");
+    });
+}
---
+++ src/lib.rs
@@ -0,0 +1,256 @@
+use socket2::{Domain, SockAddr, Socket, Type};
+
+use std::sync::{Arc, Mutex};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {}
+}
+
+pub async fn initial() -> tokio::sync::broadcast::Sender<()> {
+    let addrs = vec![
+        "127.0.0.1:19208".parse().unwrap(),
+        "127.0.0.1:19210".parse().unwrap(),
+        //"192.168.200.3:19209".parse().unwrap(),
+        // "192.168.200.3:19210".parse().unwrap(),
+        //"10.0.0.1:19209".parse().unwrap(),
+    ];
+
+    let addrs_2 = vec![
+        "127.0.0.1:19211".parse().unwrap(),
+        "127.0.0.1:19212".parse().unwrap(),
+        //"192.168.200.3:19201".parse().unwrap(),
+        // "192.168.200.3:19210".parse().unwrap(),
+        //"10.0.0.1:19209".parse().unwrap(),
+    ];
+
+    // let addrs = Arc::new(addrs);
+    // let socket = Arc::new(socket);
+    // let sender = Arc::new(sender);
+    // let mut buf = Box::new([0u8; 65535]);
+    // let mut count = 0usize;
+
+    let (tx, mut rx) = tokio::sync::broadcast::channel(1);
+
+    let mut dis_obj = Distributor::new("127.0.0.1:5503".parse().unwrap(), addrs, tx.clone());
+    let mut dis_obj_2 = Distributor::new("127.0.0.1:19210".parse().unwrap(), addrs_2, tx.clone());
+    let mut dis_vec = vec![dis_obj, dis_obj_2];
+    let map = generate_sender_map(&mut dis_vec, tx.clone());
+
+    for dis_obj in dis_vec.drain(..) {
+        dis_obj.run(map.clone()).await;
+    }
+
+    tx
+}
+
+pub fn generate_socket(
+    bind_addr: std::net::SocketAddr,
+    recv_buff_size: usize,
+    send_buff_size: usize,
+) -> tokio::net::UdpSocket {
+    let sender = Socket::new(Domain::ipv4(), Type::dgram(), None).unwrap();
+    sender.bind(&SockAddr::from(bind_addr)).unwrap();
+    sender.set_nonblocking(true).unwrap();
+    sender.set_recv_buffer_size(recv_buff_size).unwrap();
+    sender.set_send_buffer_size(send_buff_size).unwrap();
+    let sender = sender.into_udp_socket();
+    tokio::net::UdpSocket::from_std(sender).unwrap()
+}
+use std::collections::{HashMap, HashSet};
+
+pub struct Distributor {
+    pub receiver: tokio::net::UdpSocket,
+    pub recv_speed: Arc<AtomicUsize>,
+    pub recv_speed_acc: Arc<AtomicUsize>,
+    pub stop_broadcast_sender: tokio::sync::broadcast::Sender<()>,
+    pub remote: Vec<Arc<RemoteInfo>>,
+}
+
+impl Distributor {
+    pub fn new(
+        listen_addr: std::net::SocketAddr,
+        remote_addrs: Vec<std::net::SocketAddr>,
+        stop_broadcast_sender: tokio::sync::broadcast::Sender<()>,
+    ) -> Self {
+        Self {
+            receiver: generate_socket(listen_addr, 1024 * 1024, 1024),
+            recv_speed: Arc::new(AtomicUsize::new(0)),
+            recv_speed_acc: Arc::new(AtomicUsize::new(0)),
+            remote: remote_addrs
+                .iter()
+                .map(|addr| {
+                    Arc::new(RemoteInfo {
+                        addr: *addr,
+                        speed: AtomicUsize::new(0),
+                        speed_acc: AtomicUsize::new(0),
+                    })
+                })
+                .collect(),
+            stop_broadcast_sender: stop_broadcast_sender,
+        }
+    }
+
+    pub async fn run(
+        mut self,
+        sender_map: Arc<HashMap<std::net::SocketAddr, crossbeam::channel::Sender<SendRequest>>>,
+    ) {
+        let mut stop_broadcast_recv = self.stop_broadcast_sender.subscribe();
+
+        let recv_speed = self.recv_speed.clone();
+        let recv_speed_acc = self.recv_speed_acc.clone();
+
+        let local_addr = self.receiver.local_addr().unwrap();
+
+        let remotes = self.remote.clone();
+        let mut buf = Box::new([0u8; 65536]);
+
+        // receive thread
+        tokio::spawn(async move {
+            tokio::select! {
+                _ = async  {
+                        loop {
+                            //println!("hhh");
+                        if let Ok((len, _)) = self.receiver.recv_from(&mut buf[..]).await{
+                            self.recv_speed_acc.fetch_add(len, Ordering::SeqCst);
+                            let data = Arc::new(buf[..len].to_vec());
+                            for remote in &self.remote {
+                                sender_map
+                                    .get(&remote.addr)
+                                    .unwrap()
+                                    .send(SendRequest::new(remote.clone(), data.clone()))
+                                    .unwrap();
+                            }
+                        }
+
+                }} => { },
+
+                 // caculate speed
+            _ = async  {
+                let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(1_000));
+                loop{
+                    interval.tick().await;
+                    recv_speed.store(recv_speed_acc.load(Ordering::SeqCst), Ordering::SeqCst);
+                    recv_speed_acc.store(0, Ordering::SeqCst);
+                    println!("recv: {} bps", 8 * recv_speed.load(Ordering::SeqCst));
+                    for remote in &remotes {
+                        remote
+                            .speed
+                            .store(remote.speed_acc.load(Ordering::SeqCst), Ordering::SeqCst);
+                        remote.speed_acc.store(0, Ordering::SeqCst);
+                        println!(
+                            "{:?}: {} bps",
+                            remote.addr,
+                            8 * remote.speed.load(Ordering::SeqCst)
+                        );
+                    }
+                }
+            } => {},
+            _ = stop_broadcast_recv.recv() => { println!("{} is closed!", local_addr) },
+            };
+        });
+    }
+}
+
+pub fn generate_sender_map(
+    distributors: &mut Vec<Distributor>,
+    stop_broadcast_sender: tokio::sync::broadcast::Sender<()>,
+) -> Arc<HashMap<std::net::SocketAddr, crossbeam::channel::Sender<SendRequest>>> {
+    let mut map = HashMap::new();
+    let mut local_ips = HashMap::new();
+    // It will choose the same sender port for every net card
+    let mut socket = std::net::UdpSocket::bind("0.0.0.0:0").unwrap();
+    for item in distributors {
+        for remote in &mut item.remote {
+            socket.connect(remote.addr).unwrap();
+            let local = socket.local_addr().unwrap();
+
+            if !local_ips.contains_key(&local) {
+                local_ips.insert(local, generate_sender_thread(stop_broadcast_sender.clone()));
+            }
+            map.insert(remote.addr, local_ips.get(&local).unwrap().clone());
+        }
+    }
+
+    println!("{:?}", local_ips);
+
+    Arc::new(map)
+}
+
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+
+pub struct RemoteInfo {
+    pub addr: std::net::SocketAddr,
+    pub speed: AtomicUsize,
+    pub speed_acc: AtomicUsize,
+}
+
+pub struct SendRequest {
+    pub remote: Arc<RemoteInfo>,
+    pub data: Arc<Vec<u8>>,
+}
+impl SendRequest {
+    pub fn new(remote: Arc<RemoteInfo>, data: Arc<Vec<u8>>) -> Self {
+        Self { remote, data }
+    }
+}
+
+pub fn generate_sender_thread(
+    mut stop_broadcast_sender: tokio::sync::broadcast::Sender<()>,
+) -> crossbeam::channel::Sender<SendRequest> {
+    let (tx, rx) = crossbeam::channel::unbounded::<SendRequest>();
+    let socket = generate_socket("0.0.0.0:0".parse().unwrap(), 1024, 3 * 1024 * 1024);
+
+    let rrx = rx.clone();
+
+    let mut stop_for_send_thread = stop_broadcast_sender.subscribe();
+
+    tokio::spawn(async move {
+        tokio::select! {
+            _ = async {
+                loop {
+                if let Ok(req) = rx.recv() {
+                    let len = socket
+                        .send_to(&req.data[..], req.remote.addr)
+                        .await
+                        .unwrap();
+                    req.remote.speed_acc.fetch_add(len, Ordering::SeqCst);
+                }
+                else{
+                    break;
+                }
+            }
+            }=>{},
+            _ = async{
+                let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(500));
+
+                let mut flag_count = 0usize;
+                interval.tick().await;
+                interval.tick().await;
+                loop {
+                    let count_last = rrx.len();
+                    interval.tick().await;
+                    let count = rrx.len();
+                    if count > count_last {
+                        if flag_count > 3 {
+                            println!("buffer len: {}", count);
+                            for i in 0..count / 2 {
+                                rrx.try_recv();
+                            }
+                            flag_count = 4;
+                        }
+                        flag_count += 1;
+                    } else {
+                        flag_count = 0;
+                    }
+                }
+            } => {},
+            _ = async {stop_for_send_thread.recv().await;} =>{}
+        }
+    });
+
+    tx
+}
