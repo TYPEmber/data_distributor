1b6d2544a697a5b148dbae13f4256d7ce3ed2f20
Amadeus <nono02200059@gmail.com>
2021-03-11 12:51:14 +0800

    impl AsyncLogger

--- src/logger.rs
+++ src/logger.rs
@@ -1,6 +1,23 @@
 use log::{Level, Metadata, Record};
+use std::sync::Arc;
 
-struct AsyncLogger;
+struct AsyncLogger {
+    tx: tokio::sync::broadcast::Sender<String>,
+}
+
+impl AsyncLogger {
+    pub fn new() -> Self {
+        let (tx, _) = tokio::sync::broadcast::channel::<String>(20);
+
+        let mut rx0 = tx.subscribe();
+        tokio::spawn(async move {
+            loop {
+                println!("{}", rx0.recv().await.unwrap_or("slow".to_string()));
+            }
+        });
+        AsyncLogger { tx }
+    }
+}
 
 impl log::Log for AsyncLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
@@ -10,18 +27,20 @@ impl log::Log for AsyncLogger {
     fn log(&self, record: &Record) {
         //println!("{:?}", record);
         if self.enabled(record.metadata()) {
-            if record.level() == Level::Error {
-                println!(
-                    "[{}]{}[{}][{}][{}]",
-                    record.level(),
-                    record.args(),
-                    record.module_path().unwrap_or("<unamed>"),
-                    record.file().unwrap_or("<unamed>"),
-                    record.line().unwrap_or(0)
-                );
-            } else {
-                println!("[{}]{}", record.level(), record.args());
-            }
+            self.tx
+                .send(if record.level() == Level::Error {
+                    format!(
+                        "[{}]{}[{}][{}][{}]",
+                        record.level(),
+                        record.args(),
+                        record.module_path().unwrap_or("<unamed>"),
+                        record.file().unwrap_or("<unamed>"),
+                        record.line().unwrap_or(0)
+                    )
+                } else {
+                    format!("[{}]{}", record.level(), record.args())
+                })
+                .unwrap();
         }
     }
 
@@ -30,8 +49,10 @@ impl log::Log for AsyncLogger {
 
 use log::{LevelFilter, SetLoggerError};
 
-static LOGGER: AsyncLogger = AsyncLogger;
+lazy_static::lazy_static! {
+    static  ref LOGGER: AsyncLogger = AsyncLogger::new();
+}
 
 pub fn init() -> Result<(), SetLoggerError> {
-    log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Info))
+    log::set_logger(&*LOGGER).map(|()| log::set_max_level(LevelFilter::Info))
 }
